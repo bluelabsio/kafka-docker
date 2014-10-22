bbafca0ffbeebe5122d50ace2a257be7ef367487
Chelsea Zhang <zhang.chelsea@gmail.com>
2014-10-22 14:20:56 -0400

    allow setting advertised hostname externally, so non-docker ip can be used

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
 HOST_IP=$(hostname -i)
+# if advertised hostname is not already set, fall back to host ip
+ADVERTISED_HOSTNAME=${ADVERTISED_HOSTNAME:-HOST_IP}
 
 sed -r -i "s/(zookeeper.connect)=(.*)/\1=$ZK_PORT_2181_TCP_ADDR/g" $KAFKA_HOME/config/server.properties
 sed -r -i "s/(broker.id)=(.*)/\1=$BROKER_ID/g" $KAFKA_HOME/config/server.properties
-sed -r -i "s/#(advertised.host.name)=(.*)/\1=$HOST_IP/g" $KAFKA_HOME/config/server.properties
+sed -r -i "s/#(advertised.host.name)=(.*)/\1=$ADVERTISED_HOSTNAME/g" $KAFKA_HOME/config/server.properties
 sed -r -i "s/^(port)=(.*)/\1=$PORT/g" $KAFKA_HOME/config/server.properties
 
 if [ "$KAFKA_HEAP_OPTS" != "" ]; then
