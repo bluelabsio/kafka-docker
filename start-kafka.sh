e8abd0c3707f6134f8be601bf6d71dbd9b6c2627
Chelsea Zhang <zhang.chelsea@gmail.com>
2014-10-22 16:32:38 -0400

    try setting host.name instead

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-HOST_IP=$(hostname -i)
-# if advertised hostname is not already set, fall back to host ip
-ADVERTISED_HOSTNAME=${ADVERTISED_HOSTNAME:-HOST_IP}
+# if host ip is not explicitly set, retrieve it
+HOST_IP=${HOST_IP:-$(hostname -i)}
 
 sed -r -i "s/(zookeeper.connect)=(.*)/\1=$ZK_PORT_2181_TCP_ADDR/g" $KAFKA_HOME/config/server.properties
 sed -r -i "s/(broker.id)=(.*)/\1=$BROKER_ID/g" $KAFKA_HOME/config/server.properties
-sed -r -i "s/#(advertised.host.name)=(.*)/\1=$ADVERTISED_HOSTNAME/g" $KAFKA_HOME/config/server.properties
+sed -r -i "s/#(host.name)=(.*)/\1=$HOST_IP/g" $KAFKA_HOME/config/server.properties
 sed -r -i "s/^(port)=(.*)/\1=$PORT/g" $KAFKA_HOME/config/server.properties
 
 if [ "$KAFKA_HEAP_OPTS" != "" ]; then
